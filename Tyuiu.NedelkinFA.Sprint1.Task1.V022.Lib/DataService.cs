927d452c0c5a398b11161d782917a5a17ed5cc32
Федя Неделькин <fffedosss898@gmail.com>
2024-09-22 17:13:32 +0500

    DA

--- Tyuiu.NedelkinFA.Sprint1.Task1.V022.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint1.Task1.V022.Lib/DataService.cs
@@ -1,7 +1,12 @@
-﻿namespace Tyuiu.NedelkinFA.Sprint1.Task1.V022.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
+namespace Tyuiu.NedelkinFA.Sprint1.Task1.V022.Lib
+{
+    public class DataService : ISprint1Task1V22
+    {
+        public double Calculate(double x, double y)
+        {
+            return (5 + x) / (y * x);
+        }
     }
 }
--- Tyuiu.NedelkinFA.Sprint1.Task1.V022.Test/DataServiceTest.cs
+++ Tyuiu.NedelkinFA.Sprint1.Task1.V022.Test/DataServiceTest.cs
@@ -1,6 +1,6 @@
-using Tyuiu.NedelkinFA.Sprint1.Task1.V22.Lib;
+using Tyuiu.NedelkinFA.Sprint1.Task1.V022.Lib;
 
-namespace Tyuiu.NedelkinFA.Sprint1.Task1.V22.Test
+namespace Tyuiu.NedelkinFA.Sprint1.Task1.V022.Test
 {
     [TestClass]
     public class DataServiceTest
@@ -12,7 +12,7 @@ namespace Tyuiu.NedelkinFA.Sprint1.Task1.V22.Test
             double x = 1.0;
             double y = 2.0;
             var res = ds.Calculate(x, y);
-            Assert.AreEqual(-1, res);
+            Assert.AreEqual(3, res);
         }
     }
 }
