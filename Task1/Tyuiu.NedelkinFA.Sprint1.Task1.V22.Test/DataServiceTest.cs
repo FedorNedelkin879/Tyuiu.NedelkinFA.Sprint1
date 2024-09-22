a0f0ac845b154b653488f2c2e8b2b21d81246d6b
Федя Неделькин <fffedosss898@gmail.com>
2024-09-22 16:23:25 +0500

    V22

---
+++ Task1/Tyuiu.NedelkinFA.Sprint1.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.NedelkinFA.Sprint1.Task1.V22.Lib;
+
+namespace Tyuiu.NedelkinFA.Sprint1.Task1.V22.Test
+{
+    [TestClass]
+    public class DataServiceTEst
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
