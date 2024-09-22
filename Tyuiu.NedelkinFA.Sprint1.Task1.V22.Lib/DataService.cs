2eae44cd5ee26e013959780530297b5796b396d7
Федя Неделькин <fffedosss898@gmail.com>
2024-09-22 17:01:37 +0500

    wsadfryjfg

--- Tyuiu.NedelkinFA.Sprint1.Task1.V22.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint1.Task1.V22.Lib/DataService.cs
@@ -2,7 +2,7 @@
 
 namespace Tyuiu.NedelkinFA.Sprint1.Task1.V22.Lib
 {
-    public class DataService : ISprint1Task1V22
+    public class DataService : ISprint1Task1V0
     {
         public double Calculate(double x, double y)
         {
