5a09ec4bbee5e0e56ea1f30d092ac4d94e44f395
Федя Неделькин <fffedosss898@gmail.com>
2024-10-04 23:12:58 +0500

    cbxb

--- Tyuiu.NedelkinFA.Sprint1.Task5.V3.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint1.Task5.V3.Lib/DataService.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.NedelkinFA.Sprint1.Task5.V3.Lib
         public int Calculate(int k)
         {
 
-            int h = (k / 10000) % 100;
-            return h;
+            int res = (k / 100) % 10;
+            return res;
         }
     }
 }
