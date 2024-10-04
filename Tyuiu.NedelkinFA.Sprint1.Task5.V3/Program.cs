39d2d383729c783526fc4b447c0fc1faaf821171
Федя Неделькин <fffedosss898@gmail.com>
2024-10-05 00:09:55 +0500

    цу5к7ен\гшзщ

--- Tyuiu.NedelkinFA.Sprint1.Task5.V3/Program.cs
+++ Tyuiu.NedelkinFA.Sprint1.Task5.V3/Program.cs
@@ -6,14 +6,11 @@ namespace ThirdDigitFromEnd
     {
         static void Main(string[] args)
         {
-            // Ввод положительного целого числа
             Console.WriteLine("Введите положительное целое число:");
             int k = Convert.ToInt32(Console.ReadLine());
 
-            // Присвоение третьей от конца цифры переменной h
             int h = GetThirdDigitFromEnd(k);
 
-            // Вывод результата
             if (h != -1)
             {
                 Console.WriteLine($"Третья от конца цифра числа {k} равна {h}.");
@@ -26,13 +23,11 @@ namespace ThirdDigitFromEnd
 
         static int GetThirdDigitFromEnd(int number)
         {
-            // Проверка, что число имеет хотя бы три цифры
             if (number < 100)
             {
-                return -1; // Возвращаем -1, если цифр меньше трех
+                return -1;
             }
 
-            // Извлечение третьей от конца цифры
             return (number / 100) % 10;
         }
     }
