f2082f462ab179dc8e51ae492897866d724ddc54
Федя Неделькин <fffedosss898@gmail.com>
2024-10-05 00:38:23 +0500

    aerstjkhgj,mbn

---
+++ Tyuiu.NedelkinFA.Sprint1.Task7.V17.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.NedelkinFA.Sprint1.Task7.V17.Lib
+{
+    public class DataService : ISprint1Task7V17
+    {
+        public double Calculate(double x, double y)
+        {
+            double numerator = 1 + Math.Sin(Math.Sqrt(x * x + 1)); 
+            double denominator = Math.Cos(12 * y - 4);
+
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException("Знаменатель равен нулю. Проверьте значение y.");
+            }
+
+            return numerator / denominator;
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint1.Task7.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.NedelkinFA.Sprint1.Task7.V17.Lib;
+namespace Tyuiu.NedelkinFA.Sprint1.Task7.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 2;
+            double wait = 0.75;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
