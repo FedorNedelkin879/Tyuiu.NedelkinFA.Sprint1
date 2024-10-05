c18fe2f624a40fd44a1cc3432f1df900bbb5df2f
Федя Неделькин <fffedosss898@gmail.com>
2024-10-05 11:47:19 +0500

    вапв

--- Tyuiu.NedelkinFA.Sprint1.Task5.V3/Program.cs
+++ Tyuiu.NedelkinFA.Sprint1.Task5.V3/Program.cs
@@ -6,24 +6,12 @@ namespace ThirdDigitFromEnd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введитe целое число:");
+            Console.WriteLine("Введите целое число:");
             int k = Convert.ToInt32(Console.ReadLine());
             int h = GetThirdDigitFromEnd(k);
-            if (h != -1)
-            {
-                Console.WriteLine($"Третья c конца цифра - {h}.");
-            }
-         
+            if (h != -1) Console.WriteLine($" {h}.");
         }
 
-        static int GetThirdDigitFromEnd(int number)
-        {
-            if (number < 100)
-            {
-                return -1;
-            }
-
-            return (number / 100) % 10;
-        }
+        static int GetThirdDigitFromEnd(int number) => number < 100 ? -1 : (number / 100) % 10;
     }
 }
